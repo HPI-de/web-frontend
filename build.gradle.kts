4ba2cd0b82bdc4e319b7ecd446de7781efd5c3d1
Maximilian Stiede <maximilian.stiede@student.hpi.uni-potsdam.de>
2019-10-23 22:39:09 +0200

    feat: initial ktor

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+import org.jetbrains.kotlin.gradle.dsl.Coroutines
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+val jvmTarget: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.3.50"
+}
+
+group = "de.hpi.cloud.web-frontend"
+version = "0.0.1"
+
+application {
+    mainClassName = "io.ktor.server.jetty.EngineMain"
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    jcenter()
+    maven { url = uri("https://kotlin.bintray.com/ktor") }
+    maven { url = uri("https://dl.bintray.com/kotlin/kotlin-eap") }
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
+    implementation("io.ktor:ktor-server-jetty:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-server-host-common:$ktor_version")
+    implementation("io.ktor:ktor-metrics:$ktor_version")
+    implementation("io.ktor:ktor-auth:$ktor_version")
+    implementation("io.ktor:ktor-auth-jwt:$ktor_version")
+    implementation("io.ktor:ktor-jackson:$ktor_version")
+    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = jvmTarget
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = jvmTarget
+    }
+}
---
+++ src/main/kotlin/de/hpi/cloud/webfrontend/Application.kt
@@ -0,0 +1,131 @@
+package de.hpi.cloud.webfrontend
+
+import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.auth.Authentication
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.http.content.CachingOptions
+import io.ktor.http.content.resources
+import io.ktor.http.content.static
+import io.ktor.jackson.jackson
+import io.ktor.request.path
+import io.ktor.response.respond
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.util.date.GMTDate
+import org.slf4j.event.Level
+
+fun main(args: Array<String>): Unit = io.ktor.server.jetty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(Compression) {
+        gzip {
+            priority = 1.0
+        }
+        deflate {
+            priority = 10.0
+            minimumSize(1024) // condition
+        }
+    }
+
+    install(CallLogging) {
+        level = Level.INFO
+        filter { call -> call.request.path().startsWith("/") }
+    }
+
+    install(ConditionalHeaders)
+
+    install(CORS) {
+        method(HttpMethod.Options)
+        method(HttpMethod.Put)
+        method(HttpMethod.Delete)
+        method(HttpMethod.Patch)
+        header(HttpHeaders.Authorization)
+        header("MyCustomHeader")
+        allowCredentials = true
+        if (testing) {
+            anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+        }
+    }
+
+    install(CachingHeaders) {
+        options { outgoingContent ->
+            when (outgoingContent.contentType?.withoutParameters()) {
+                ContentType.Text.CSS -> CachingOptions(
+                    CacheControl.MaxAge(maxAgeSeconds = 24 * 60 * 60),
+                    expires = null as? GMTDate?
+                )
+                else -> null
+            }
+        }
+    }
+
+    install(DefaultHeaders) {
+        // anonymize server
+        header("Server", "httpd 3.0A")
+    }
+
+    install(HSTS) {
+        includeSubDomains = true
+    }
+
+    // https://ktor.io/servers/features/https-redirect.html#testing
+    if (!testing) {
+        install(HttpsRedirect) {
+            // The port to redirect to. By default 443, the default HTTPS port.
+            sslPort = 443
+            // 301 Moved Permanently, or 302 Found redirect.
+            permanentRedirect = true
+        }
+    }
+
+    install(Authentication) {
+    }
+
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+        get("/ktor") {
+            call.respondText(
+                "<!DOCTYPE html><head><title>Home</title></head><body><h1>Hello Ktor!</h1><img src=\"/static/ktor_logo.svg\"/></body></html>",
+                contentType = ContentType.Text.Html
+            )
+        }
+
+        // Static feature. Try to access `/static/ktor_logo.svg`
+        static("/static") {
+            resources("static")
+        }
+
+        install(StatusPages) {
+            exception<AuthenticationException> { cause ->
+                call.respond(HttpStatusCode.Unauthorized)
+            }
+            exception<AuthorizationException> { cause ->
+                call.respond(HttpStatusCode.Forbidden)
+            }
+
+        }
+
+        get("/json/jackson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
+class AuthenticationException : RuntimeException()
+class AuthorizationException : RuntimeException()
+
